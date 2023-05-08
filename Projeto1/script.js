2c5dbc71881924b337217bdc03590b2b9ed29ac6
Fellipe Scruph <76188626+FellipeScrup@users.noreply.github.com>
2023-05-08 16:41:15 -0300

    Add files via upload

---
+++ Projeto1/script.js
@@ -0,0 +1,32 @@
+const wrapper  = document.querySelector('.wrapper');
+const loginLink  = document.querySelector('.login-link');
+const registerLink  = document.querySelector('.register-link');
+
+const bntPopup  = document.querySelector('.btLogin-popup');
+
+const iconClose  = document.querySelector('.icon-close');
+
+registerLink.addEventListener('click', () =>{
+    wrapper.classList.add('active');
+
+
+});
+
+loginLink.addEventListener('click', () =>{
+    wrapper.classList.remove('active');
+    
+
+});
+
+bntPopup.addEventListener('click', () =>{
+    wrapper.classList.add('active-popup');
+
+
+});
+
+iconClose.addEventListener('click', () =>{
+    wrapper.classList.remove('active-popup');
+
+
+});
+
